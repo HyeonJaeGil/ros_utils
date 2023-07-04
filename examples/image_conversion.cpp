9aba017509b7ee3a0819ade1d074e301bae28932
HyeonjaeGil <now9728@gmail.com>
2023-07-05 06:24:01 +0900

    change folder name test -> examples

--- examples/image_conversion.cpp
+++ examples/image_conversion.cpp
@@ -7,7 +7,7 @@
 
 int main() {
 
-  BagManager bag1("/home/hj/d435_2023-06-27-20-59-08.bag");
+  BagManager bag1("/home/hj/d435_2023-06-27-20-59-26.bag");
   bag1.printInfo();
 
   std::string save_path("/home/hj/" + bag1.getBagFileName());
@@ -30,21 +30,6 @@ int main() {
                  << imu_msg.angular_velocity.z << std::endl;
   }
   imu_csv_file.close();
-
-  std::vector<sensor_msgs::Image> image_msgs;
-  bag1.getMessages(image_msgs, "/camera/color/image_raw");
-  std::cout << "image_msgs.size(): " << image_msgs.size() << std::endl;
-
-  std::string image_png_path_prefix(save_path + "/rgb");
-  for (int i = 0; i < image_msgs.size(); i++) {
-    cv::Mat image = TopicHelper::rosImage2cvImage(image_msgs[i], "bgr8");
-    std::string timestamp = std::to_string(image_msgs[i].header.stamp.toNSec());
-    cv::imwrite(image_png_path_prefix + "/" + timestamp + ".png", image);
-    cv::imshow("image", image);
-    if (cv::waitKey(1) == 'q') {
-      break;
-    }
-  }
 
   std::vector<sensor_msgs::CameraInfo> camera_info_msgs;
   bag1.getMessages(camera_info_msgs, "/camera/color/camera_info");
